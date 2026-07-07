36bc1672c59520a935ea6806bdadd7ecb31c9b3f
hpcca <mengx8756@gmail.com>
2026-07-07 12:45:48 +0800

    Add depth-assisted replacement positioning

--- Assets/AR80sRetro/Scripts/ARRaycastPositionSolver.cs
+++ Assets/AR80sRetro/Scripts/ARRaycastPositionSolver.cs
@@ -11,13 +11,17 @@ namespace AR80sRetro
 
         [SerializeField] private ARRaycastManager raycastManager;
         [SerializeField] private Camera arCamera;
+        [SerializeField] private ARDepthFrameProvider depthProvider;
         [SerializeField] private TrackableType trackableTypes = TrackableType.PlaneWithinPolygon;
         [SerializeField] private Vector2 anchorInBoundingBox = new Vector2(0.5f, 0.9f);
         [SerializeField] private bool faceCamera;
+        [SerializeField, Range(0f, 1f)] private float depthHorizontalWeight = 0.75f;
+        [SerializeField, Min(0.05f)] private float maxDepthPlaneHorizontalDeltaMeters = 0.6f;
 
         private void Reset()
         {
             raycastManager = FindObjectOfType<ARRaycastManager>();
+            depthProvider = FindObjectOfType<ARDepthFrameProvider>();
             arCamera = Camera.main;
         }
 
@@ -54,6 +58,29 @@ namespace AR80sRetro
             }
 
             pose = Hits[0].pose;
+            Vector3 planePosition = pose.position;
+
+            if (depthProvider != null
+                && depthHorizontalWeight > 0f
+                && depthProvider.TrySampleWorldPoint(
+                    detection,
+                    new Vector2(0.5f, 0.5f),
+                    out Vector3 depthWorldPoint,
+                    out _,
+                    out _))
+            {
+                Vector2 planeHorizontal = new Vector2(planePosition.x, planePosition.z);
+                Vector2 depthHorizontal = new Vector2(depthWorldPoint.x, depthWorldPoint.z);
+                float horizontalDelta = Vector2.Distance(planeHorizontal, depthHorizontal);
+                float effectiveWeight = horizontalDelta > maxDepthPlaneHorizontalDeltaMeters
+                    ? depthHorizontalWeight * 0.35f
+                    : depthHorizontalWeight;
+
+                pose.position = new Vector3(
+                    Mathf.Lerp(planePosition.x, depthWorldPoint.x, effectiveWeight),
+                    planePosition.y,
+                    Mathf.Lerp(planePosition.z, depthWorldPoint.z, effectiveWeight));
+            }
 
             if (faceCamera && arCamera != null)
             {
